94d1e50ffe41ac72ff300bcec68bd4a5034ac7d2
Celeste <celestedeschamphelaere@protonmail.com>
2025-12-17 23:38:29 +0100

    ugent hpc boilerplate

---
+++ hpc/start.sh
@@ -0,0 +1,4 @@
+#script for ugent HPC
+cd /kyukon/data/gent/courses/2025/mach_learn_C003758/members/vsc45329/nemotron
+module load vsc-venv; source vsc-venv --activate --requirements ./hpc/requirements.txt --modules ./hpc/modules.txt
+HF_HOME=./hf_home python train_model.py
