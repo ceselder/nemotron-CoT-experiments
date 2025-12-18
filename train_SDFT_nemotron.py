c198077e09473bdbbf3117435ac1afe537e1c095
Celeste <celestedeschamphelaere@protonmail.com>
2025-12-18 09:30:28 +0100

    wrong model name

--- train_SDFT_nemotron.py
+++ train_SDFT_nemotron.py
@@ -8,7 +8,7 @@ dtype = None
 load_in_4bit = True
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="unsloth/Llama-3.3-Nemotron-Super-49B-v1-bnb-4bit",
+    model_name="unsloth/Llama-3_3-Nemotron-Super-49B-v1_5",
     max_seq_length=max_seq_length,
     dtype=dtype,
     load_in_4bit=load_in_4bit,
