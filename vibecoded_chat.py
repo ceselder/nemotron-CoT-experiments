9295d7dec4b18d5f3237605f564af6099dad8234
Celeste <celestedeschamphelaere@protonmail.com>
2025-12-19 21:15:00 +0100

    h100 yay

--- vibecoded_chat.py
+++ vibecoded_chat.py
@@ -32,7 +32,8 @@ def load_model():
         model_name=MODEL_PATH,
         max_seq_length=MAX_SEQ_LENGTH,
         dtype=torch.bfloat16,
-        load_in_4bit=True,
+        load_in_4bit=False,
+        load_in_8bit=True,
         trust_remote_code=True,
     )
     FastLanguageModel.for_inference(model)
